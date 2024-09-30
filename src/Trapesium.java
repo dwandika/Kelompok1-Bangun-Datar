019b6c86255cab31196bd5a97ce707eb9588c64e
fuadhi <muzainsfudh@gmail.com>
2024-09-30 15:41:37 +0700

    Fuadhi Trapesium

---
+++ src/Trapesium.java
@@ -0,0 +1,37 @@
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author coldp
+ */
+public class Trapesium {
+    int sisiA,sisiB,Tinggi;
+    double luas,keliling;
+    
+    public void setsisiA(int sisiA){
+        this.sisiA = sisiA;
+    }
+    public void setsisiB(int sisiB){
+        this.sisiB = sisiB;
+    } 
+    public void setTinggi(int Tinggi){
+        this.Tinggi = Tinggi;
+    }
+    public void setkeliling(){
+        this.keliling = sisiA + sisiB + sisiA + sisiB;
+    }  
+    public void setluas(){
+        this.luas = 0.5*(sisiA+sisiB)*Tinggi;
+    }
+    public double getluas(){
+        return luas;
+    }
+    public double getkeliling(){
+        return keliling;
+    }
+}
