085d4925946b877c292c988aba31b86716f73181
shifa <nuriyahshifaaun@gmail.com>
2024-10-14 15:47:47 +0700

    extend Persegi

--- src/frameBangundatar/framePersegi.java
+++ src/frameBangundatar/framePersegi.java
@@ -184,13 +184,13 @@ public class framePersegi extends javax.swing.JFrame {
         pr.setluas();
         pr.setkeliling();
 
-        vLuas.setText(String.valueOf(pr.getluas()));
-        vKeliling.setText(String.valueOf(pr.getkeliling()));
+        vLuas.setText(String.valueOf(pr.getLuas()));
+        vKeliling.setText(String.valueOf(pr.getKeliling()));
     }//GEN-LAST:event_bHitungActionPerformed
 
     private void bBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bBackActionPerformed
         dispose();
-        new mainFrame().setVisible(true);
+       // new mainFrame().setVisible(true);
     }//GEN-LAST:event_bBackActionPerformed
 
     /**
